6f0a505bd5df3e9f559a79f0dc2827f9c42bf5fd
JZSweet <sweet_zhaozhao@live.cn>
2020-12-16 14:18:51 -0800

    new update

--- jcript.js
+++ jcript.js
@@ -4,26 +4,24 @@ $("#currentDay").append(now);
 // add eventlisner
 // local Storage(save after refreash the page)
 $(document).ready(function () {
-    // $("#9Am").on("click", function (event) {
-    //     event.preventDefault();
-    //     console.log("click")
-    //     storageEven(".block-entry", "to-do");
-    // });
-
-    // var noteOutput = localStorage.getItem("to-do");
-    // $(".block-entry").val(noteOutput);
-
-    // $("#10Am").on("click", function (event) {
-    //     event.preventDefault();
-    //     console.log("click")
-    //     storageEven(".block-entry1", "to-do1");
-    // });
-    getTodo(".block-entry1", "to-do1")
     getTodo(".block-entry", "to-do")
+    getTodo(".block-entry1", "to-do1")
     getTodo(".block-entry2", "to-do2")
+    getTodo(".block-entry3", "to-do3")
+    getTodo(".block-entry4", "to-do4")
+    getTodo(".block-entry5", "to-do5")
+    getTodo(".block-entry6", "to-do6")
+    getTodo(".block-entry7", "to-do7")
+    getTodo(".block-entry8", "to-do8")
     buttonName("#9Am",".block-entry","to-do")
     buttonName("#10Am",".block-entry1","to-do1")
     buttonName("#11Am",".block-entry2","to-do2")
+    buttonName("#12Pm",".block-entry3","to-do3")
+    buttonName("#1Pm",".block-entry4","to-do4")
+    buttonName("#2Pm",".block-entry5","to-do5")
+    buttonName("#3Pm",".block-entry6","to-do6")
+    buttonName("#4Pm",".block-entry7","to-do7")
+    buttonName("#5Pm",".block-entry8","to-do8")
 });
 
 // rows color-coded to indicate past, present, or future
